a1cd7b7ba4f437ee51d5dce346e2544256e84295
Axel Iván Arroyo Lara <156393845+AxelIvanArroyoLara@users.noreply.github.com>
2026-05-11 09:21:46 -0600

    Revert "Dev 5 aaaaaaaaaaaaaaaaah"

--- Dev 5/LatentModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.equipo.documentbase.persistence;
-
-import com.equipo.documentbase.domain.LatentSpaceModel;
-import java.util.Optional;
-
-public interface LatentModelRepository {
-    void save(LatentSpaceModel model);
-    Optional<LatentSpaceModel> loadLatest();
-}
